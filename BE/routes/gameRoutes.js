44e1e6ec1738b7ca961a0a55dfad74933a0e068a
khyatig0206 <khyatig0206@gmail.com>
2025-01-12 13:12:12 +0530

    added api to get gamedata by multiple codes

--- BE/routes/gameRoutes.js
+++ BE/routes/gameRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getGameLogs,UserGameResults,getGameResultsByUserId,getUserGameDatabyCode } from "../controllers/gameController.js";
+import { getGameLogs,UserGameResults,getGameResultsByUserId,getUserGameDatabyCode,getUserGameDatabyCodes } from "../controllers/gameController.js";
 import authenticateToken from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -8,4 +8,5 @@ router.get("/getlogs",getGameLogs)
 router.get("/getGameResults",authenticateToken,UserGameResults)
 router.get("/getGameResults-by-id",getGameResultsByUserId);
 router.post("/getGameResultsbyCode",getUserGameDatabyCode);
+router.post("/getGameResultsbyCodes",getUserGameDatabyCodes);
 export default router;
